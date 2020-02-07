315eec88e50189f1d084cf0380e9b5a65d121cd6
Umbrella7D <60515903+Umbrella7D@users.noreply.github.com>
2020-02-07 14:12:56 +0000

    Add files via upload
    Explosion chance, explo destroys crate, secondary proj spawn better, removed in air projectile attempt

--- AirDropBomb/Scripts/EntitySupplyCrateBomb.cs
+++ AirDropBomb/Scripts/EntitySupplyCrateBomb.cs
@@ -1,51 +1,58 @@
 using System;
-using System.IO;
-using UnityEngine;
+using System.Collections;
 
 public class EntitySupplyCrateBomb : EntitySupplyCrate {
 
-    protected override void fallHitGround(float _v, Vector3 _fallMotion) {
-        base.fallHitGround(Mathf.Min(_v, 5f), new Vector3(_fallMotion.x, Mathf.Max(-0.75f, _fallMotion.y), _fallMotion.z));
+    private static float explodeChance = 0.5f;
+    private int closestId = -1; // = world.GetClosestPlayer(position.x, position.y, position.z, 0, -1.0);
+    protected override void fallHitGround(float _v, Vector3 _fallMotion) {  
+        /* Probably called once upon impact. I should avoid testing below block myself */
+        base.fallHitGround(Mathf.Min(_v, 5f), new Vector3(_fallMotion.x, Mathf.Max(-0.75f, _fallMotion.y), _fallMotion.z)); 
+
         Vector3 pos = this.GetPosition();
-        World world = GameManager.Instance.World;
+        World world = this.world; // GameManager.Instance.World;
+        if (this.closestId == -1) {
+            EntityPlayer _p = world.GetClosestPlayer(this.GetPosition(), 100f, false);
+            if (_p != null) this.closestId = _p.entityId;
+        }
+        float u;
         for (int k=0; k<1; k++) {            
             Vector3i where = new Vector3i(Utils.Fastfloor(pos.x), Utils.Fastfloor(pos.y) - 1 - k,  Utils.Fastfloor(pos.z));  
             BlockValue bv = world.GetBlock(where);
             if (bv.type != BlockValue.Air.type) {
-                this.Explode();
+                u = AIAirDrop.controller.Random.RandomRange(0f,1f);
+                if (u < explodeChance) GameManager.Instance.StartCoroutine(this.Explode());
                 return;
             }
-        }
-        GameRandom random = AIAirDrop.controller.Random;
-        ItemClass itemClass = ItemClass.GetItemClass("thrownAmmoMolotovCocktail", false);  
-        float u = AIAirDrop.controller.Random.RandomRange(0f,1f);
-        if (u < 0.1) {
-            Vector3 around = pos + new Vector3(random.RandomRange(-5f, 5f), random.RandomRange(-3f, -1f), random.RandomRange(-5f, 5f));           
-            GameManager.Instance.ExplosionServer(0, around, V3i(around), Quaternion.identity, new ExplosionData(itemClass.Properties), -1, 0.1f, false, null); // try -1      
         }
 	}
 
     
-    private void Explode() {		
+    private IEnumerator Explode() {		
         Vector3 pos = this.GetPosition();
         Vector3i where = V3i(pos);
 
         World world = GameManager.Instance.World;  
         SpawnItem(pos, "thrownAmmoMolotovCocktail", new Vector3(0f,-0.1f,0f)); 
-        // world.SetBlockRPC(0, where, BlockValue.Air);  /// Destroy crate      
+        
+        // world.SetBlockRPC(0, where, BlockValue.Air);  /// Fails to oestroy crate - matter of V3/V3i conversion ?       
+        this.Kill(DamageResponse.New(true));
 
         GameRandom random = AIAirDrop.controller.Random;
         for (int k=0; k< 10; k++) {
             SpawnItem(
                 pos + new Vector3(random.RandomRange(-3f, 3f), random.RandomRange(1f, 3f), random.RandomRange(-3f, 3f)),
                 "rockBomb",
-                new Vector3(random.RandomRange(-5f, 5f), random.RandomRange(2f, 4f), random.RandomRange(-5f, 5f))
+                new Vector3(random.RandomRange(-5f, 5f), random.RandomRange(-2f, 2f), random.RandomRange(-5f, 5f))
             );
+            yield return new WaitForSeconds(0.2f);
             SpawnItem(
                 pos + new Vector3(random.RandomRange(-3f, 3f), random.RandomRange(1f, 3f), random.RandomRange(-3f, 3f)),
                 "thrownAmmoMolotovCocktail",
                 new Vector3(random.RandomRange(-5f, 5f), random.RandomRange(2f, 4f), random.RandomRange(-5f, 5f))
             );
+            float dt = AIAirDrop.controller.Random.RandomRange(0.1f,1f);
+            yield return new WaitForSeconds(dt);
         }
 	}
 
@@ -56,10 +63,6 @@ public class EntitySupplyCrateBomb : EntitySupplyCrate {
         EntityPlayer closest = world.GetClosestPlayer(position.x, position.y, position.z, 0, -1.0);
         Vector3 where = position; // closest.GetPosition();
         Debug.Log(String.Format("Closest {0} {1}", closest, where));
-        //where.x = where.x + AIAirDrop.controller.Random.RandomRange(-20f, 20f);
-        //where.z = where.z + AIAirDrop.controller.Random.RandomRange(-20f, 20f);
-        //where.y = 253f;
-        // where.y = where.y + 20;
         ItemStack itemStack = new ItemStack(ItemClass.GetItem(item, false), 1); // this.CreateItemCount
         world.gameManager.ItemDropServer(
             itemStack,
